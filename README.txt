0ed49ce216fc17c1fe0e21db0b88bd926c132c32
Can Candan <ccandan3@gatech.edu>
2019-11-23 20:57:04 +0300

    update

---
+++ README.txt
@@ -0,0 +1,10 @@
+1) Checkout from https://github.com/cancandan/CS7641_A4
+2) Open and run the notebooks, the relevant ones are:
+FrozenLake.ipynb
+FrozenLake-PI-VI-LowTheta.ipynb
+Gambler-Q2M.ipynb
+Gambler-Q4M.ipynb
+Gambler-Q10M.ipynb
+Gambler-QDifferent.ipynb
+
+the notebooks are making use of the code in smallmdp.py and util.py
--- gymEnvs/__init__.py
+++ gymEnvs/__init__.py
@@ -3,11 +3,5 @@ from gym.envs.registration import register
 
 
 from .my_frozen_lake import *
-from .jack import *
-from .hanoi import *
-from .nchain import *
-from .guess import *
-from .simple import *
-from .inventory import *
 
-__all__ = ['MyFrozenLakeEnv','JacksCarRental','HanoiEnv','NChainEnv','GuessingGame','Simple','InventoryEnv']
+__all__ = ['MyFrozenLakeEnv']
